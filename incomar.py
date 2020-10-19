a80137efe00c45b299819fc5fb92530188e8e55b
Amad Muhammad <46407172+AmadMuhammad@users.noreply.github.com>
2020-10-20 01:45:56 +0500

    adding snap the curser to center

--- incomar.py
+++ incomar.py
@@ -21,6 +21,7 @@ class OBJECT_OT_step2(bpy.types.Operator):
         bpy.ops.object.delete(use_global=True)
         bpy.ops.object.select_all(action='SELECT')
         bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY')
+        bpy.ops.view3d.snap_cursor_to_center()
         bpy.ops.view3d.snap_selected_to_cursor(use_offset=True)
         bpy.ops.transform.resize(value=(0.03, 0.03, 0.03), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
         bpy.ops.transform.rotate(value=1.5708, orient_axis='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
